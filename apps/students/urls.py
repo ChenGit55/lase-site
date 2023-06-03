ce1fbe68c9c58638e90b818bfeb7c0e307a21892
Chen <chenpsilva@gmail.com>
2023-06-03 14:21:40 -0300

    edit student page created

--- apps/students/urls.py
+++ apps/students/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
     student_view,
     student_detail_view,
+    student_edit_view,
     student_create_view,
     student_search_view,
     enroll_success_view,
@@ -10,6 +11,7 @@ from .views import (
 urlpatterns = [
     path('', student_view, name= 'students'),
     path('details/<slug:slug>', student_detail_view, name='details'),
+    path('edit/<slug:slug>', student_edit_view, name='edit'),
     path('enroll/', student_create_view, name='enroll'),
     path('enroll-success/', enroll_success_view, name='enroll-success'),
     path("search/", student_search_view, name="student-search"),
--- apps/students/views.py
+++ apps/students/views.py
@@ -4,6 +4,7 @@ from .models import Student, Statistic
 from .forms import StudentForm, StatisticForm
 from .utils import superuser_required
 from django.contrib import messages
+from django.urls import reverse
 
 def student_search_view(request):
     query_dic = request.GET #get the input text by the name
@@ -41,7 +42,25 @@ def student_create_view(request):
         context['form'] = StudentForm() #reinitializing form
         messages.success(request, 'Student enrolled successfully!')
         return redirect('enroll-success')
-    return render(request, 'enroll.html', context=context)
+    return render(request, 'enroll.html', context)
+
+def student_edit_view(request, slug):
+    student = Student.objects.get(slug=slug)
+    if request.method == 'POST':
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            print(form.errors)
+            form.save()
+            return redirect(reverse('details', kwargs={'slug' : student.slug}))
+        else:
+            print(form.errors)
+    else:
+        form = StudentForm(instance=student)
+        context = {
+            'student' : student,
+            'form' : form,
+        }
+    return render(request, 'student-edit.html', context)
 
 def enroll_success_view(request):
     return render(request, 'enroll-success.html', {})
